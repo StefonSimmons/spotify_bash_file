7d8d1a58e76676218c0a5049303525bd0816d6a5
StefonSimmons <57869435+StefonSimmons@users.noreply.github.com>
2021-07-10 14:12:03 -0400

    Update spotify.sh

--- spotify.sh
+++ spotify.sh
@@ -1,3 +1,4 @@
+# FOR BASH
 function spotify (){
     DIR=#<absolute path to folder of create_playlist module>
     if [ "$1" == 'cp' ]; then
@@ -12,4 +13,24 @@ function spotify (){
     elif [ "$1" == "play" ] || [ "$1" == "pause" ]; then
         python $DIR/create_playlist.py $1
     fi
+}
+
+# =========== OR =============
+
+
+# FOR ZSH
+function spotify (){
+  DIR=#<absolute path to folder of create_playlist module>
+  if [[ "$1" == 'cp' ]]; then
+    echo "Creating $2..."
+    python $DIR/create_playlist.py $1 $2 $3
+  elif [[ "$1" == "s" ]]; then
+    echo "Searching for $2..."
+    python $DIR/create_playlist.py $1 $2
+  elif [[ "$1" == "list" ]]; then
+    echo "Getting Playlists..."
+    python $DIR/create_playlist.py $1
+  elif [[ "$1" == "play" ]] || [[ "$1" == "pause" ]]; then
+    python $DIR/create_playlist.py $1
+  fi
 }
