e86f266926fd020e8ed3686f050777e48b3b77e2
stefon <s.j.simmons@spartans.nsu.edu>
2021-07-10 12:25:08 -0400

    refactored

--- spotify.sh
+++ spotify.sh
@@ -1,18 +1,15 @@
 function spotify (){
+    DIR=#<absolute path to folder of create_playlist module>
     if [ "$1" == 'cp' ]; then
         echo "Creating $2..."
-        DIR=#<absolute path to folder of create_playlist module>
         python $DIR/create_playlist.py $1 $2 $3
     elif [ "$1" == "s" ]; then
         echo "Searching for $2..."
-        DIR=#<absolute path to folder of create_playlist module>
         python $DIR/create_playlist.py $1 $2
     elif [ "$1" == "list" ]; then
         echo "Getting Playlists..."
-        DIR=#<absolute path to folder of create_playlist module>
         python $DIR/create_playlist.py $1
     elif [ "$1" == "play" ] || [ "$1" == "pause" ]; then
-        DIR=#<absolute path to folder of create_playlist module>
         python $DIR/create_playlist.py $1
     fi
 }
